de8faa49bb112b726548723698ac00ec13ddd4e5
Hector Rios <that.hector@gmail.com>
2021-10-06 17:54:41 -0500

    Piece it together

--- main.go
+++ main.go
@@ -3,41 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/hectron/fauci.d/mapbox"
 	"github.com/hectron/fauci.d/vaccines"
 )
 
 func main() {
-	//mapboxToken := os.Getenv("MAPBOX_API_TOKEN")
-	//mapboxApi := mapbox.Api{Token: mapboxToken}
-	//httpClient := &http.Client{}
-	//coordinates, err := mapboxApi.GeocodePostalCode("60640", httpClient)
+	mapboxToken := os.Getenv("MAPBOX_API_TOKEN")
+	mapboxApi := mapbox.Api{Token: mapboxToken}
+	httpClient := &http.Client{}
+	coordinates, err := mapboxApi.GeocodePostalCode("60640", httpClient)
 
-	//if err == nil {
-	//	fmt.Println(coordinates)
-	//} else {
-	//	fmt.Println(err)
-	//}
-
-	// vaccineApi := vaccines.Api{Vaccine: vaccines.Moderna}
-	// client := &http.Client{}
-	//
-	// providers, err := vaccineApi.Request(client)
-	//
-	// if err == nil {
-	// 	fmt.Println(providers)
-	// } else {
-	// 	fmt.Println("Could not load response")
-	// 	fmt.Println(err)
-	// }
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	vaccineApi := vaccines.Api{}
 	client := &http.Client{}
 
 	apiRequest := vaccines.ApiRequest{
 		Vaccine: vaccines.Moderna,
-		Lat:     41.97,
-		Long:    -87.66,
+		Lat:     coordinates.Latitude,
+		Long:    coordinates.Longitude,
 	}
 
 	providers, err := vaccineApi.Request(apiRequest, client)
